271998b82d75bea14ba13ff74edd3da42d75fb34
nila <terrakkk@outlook.com>
2019-01-29 23:10:23 +0630

    refactor:Change Componets to Stateless Components

--- src/App.js
+++ src/App.js
@@ -77,49 +77,34 @@ class Developer {
     return this.firstname + " " + this.lastname;
   }
 }
-class Search extends Component {
-  render() {
-    const { value, onChange, children } = this.props;
-    return (
-      <form>
-        {children}&nbsp;
-        <input type="Text" onChange={onChange} value={value} />
-      </form>
-    );
-  }
-}
-class Table extends Component {
-  render() {
-    const { list, pattern, onDismiss } = this.props;
-    return (
-      <div>
-        {list.filter(isSearched(pattern)).map(item => (
-          <div key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-            {/* The added section Start */}
-            <span>
-              <Button onClick={() => onDismiss(item.objectID)}>Dismiss</Button>
-            </span>
-            {/* End */}
-          </div>
-        ))}
+// Refactoring Components to Stateless Components Start
+const Search = ({ value, onChange, children }) => (
+  <form>
+    {children}&nbsp;
+    <input type="Text" onChange={onChange} value={value} />
+  </form>
+);
+const Table = ({ list, pattern, onDismiss }) => (
+  <div>
+    {list.filter(isSearched(pattern)).map(item => (
+      <div key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+        <span>
+          <Button onClick={() => onDismiss(item.objectID)}>Dismiss</Button>
+        </span>
       </div>
-    );
-  }
-}
-class Button extends Component {
-  render() {
-    const { onClick, className = "", children } = this.props;
-    return (
-      <button onClick={onClick} className={className}>
-        {children}
-      </button>
-    );
-  }
-}
+    ))}
+  </div>
+);
+const Button = ({ onClick, className, children }) => (
+  <button onClick={onClick} className={className} type="button">
+    {children}
+  </button>
+);
+//End
 export default App;
