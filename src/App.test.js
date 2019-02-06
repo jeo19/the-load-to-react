9afc8c7c9ac82de591b4ff4288b681c054210092
nila <terrakkk@outlook.com>
2019-02-06 20:51:42 +0630

    test:Create a Table shapshot (#26)

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App, { Search } from "./App";
+import App, { Search, Button, Table } from "./App";
 import renderer from "react-test-renderer";
 describe("App", () => {
   it("renders without crashing", () => {
@@ -20,6 +20,25 @@ describe("Search", () => {
   });
   test("has a valid snapshot", () => {
     const component = renderer.create(<Search />);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+describe("Table", () => {
+  const props = {
+    list: [
+      { title: "1", author: "1", num_comments: 1, points: 2, objectID: "y" },
+      { title: "2", author: "2", num_comments: 1, points: 2, objectID: "z" }
+    ]
+  };
+
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<Table {...props} />, div);
+  });
+
+  test("has a valid snapshot", () => {
+    const component = renderer.create(<Table {...props} />);
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
